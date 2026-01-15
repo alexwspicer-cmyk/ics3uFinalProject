8c4bf713607e3c1682f422be8b459756c5a7b51b
tim <tim@LAPTOP-J1UN4UD0>
2026-01-15 15:05:34 -0500

    Finished every room the Monster can move to. Added vent health, vent errors, and the ability to reboot the vents to fix the errors.

--- ics3uFinalProject/Form1.Designer.cs
+++ ics3uFinalProject/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.testOutputLabel = new System.Windows.Forms.Label();
+            this.ventTimer = new System.Windows.Forms.Timer(this.components);
+            this.ventButton = new System.Windows.Forms.Button();
+            this.audioResetButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -41,17 +44,46 @@
             // 
             // testOutputLabel
             // 
-            this.testOutputLabel.Location = new System.Drawing.Point(161, 15);
+            this.testOutputLabel.Location = new System.Drawing.Point(121, 12);
+            this.testOutputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.testOutputLabel.Name = "testOutputLabel";
-            this.testOutputLabel.Size = new System.Drawing.Size(465, 392);
+            this.testOutputLabel.Size = new System.Drawing.Size(349, 318);
             this.testOutputLabel.TabIndex = 0;
+            // 
+            // ventTimer
+            // 
+            this.ventTimer.Enabled = true;
+            this.ventTimer.Interval = 9000;
+            this.ventTimer.Tick += new System.EventHandler(this.ventTimer_Tick);
+            // 
+            // ventButton
+            // 
+            this.ventButton.Location = new System.Drawing.Point(12, 333);
+            this.ventButton.Name = "ventButton";
+            this.ventButton.Size = new System.Drawing.Size(209, 48);
+            this.ventButton.TabIndex = 1;
+            this.ventButton.Text = "RESET VENTILATION";
+            this.ventButton.UseVisualStyleBackColor = true;
+            this.ventButton.Click += new System.EventHandler(this.ventButton_Click);
+            // 
+            // audioResetButton
+            // 
+            this.audioResetButton.Location = new System.Drawing.Point(12, 400);
+            this.audioResetButton.Name = "audioResetButton";
+            this.audioResetButton.Size = new System.Drawing.Size(209, 48);
+            this.audioResetButton.TabIndex = 2;
+            this.audioResetButton.Text = "RESET AUDIO";
+            this.audioResetButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 587);
+            this.ClientSize = new System.Drawing.Size(811, 477);
+            this.Controls.Add(this.audioResetButton);
+            this.Controls.Add(this.ventButton);
             this.Controls.Add(this.testOutputLabel);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -62,6 +94,9 @@
 
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Label testOutputLabel;
+        private System.Windows.Forms.Timer ventTimer;
+        private System.Windows.Forms.Button ventButton;
+        private System.Windows.Forms.Button audioResetButton;
     }
 }
 
--- ics3uFinalProject/Form1.cs
+++ ics3uFinalProject/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,10 +19,14 @@ namespace ics3uFinalProject
     public partial class Form1 : Form
     {
         //Stats for the game
-        int aiLevel = 10; //How quickly the monster will move
+        int aiLevel = 4; //How quickly the monster will move
         int aggressive = 0; //Aggressiveness; set to 1 to move faster
         int currentRoom; //Int to know what room the monster is in
         int moveCounter = 0; //How many seconds since the monster last moved
+
+        int gameTime;
+
+        int ventHealth = 10; //Determines the health of the the vent, ventilation error happens at 0 health
 
         public Form1()
         {
@@ -49,7 +54,7 @@ namespace ics3uFinalProject
                 {
                     moveCounter = 0; //Resets move counter
                 }
-                else if ((actionSelected == 2 || actionSelected == 3) && currentRoom == 10) //Moves the character to a new room
+                else if ((actionSelected == 2 || actionSelected == 3) && currentRoom == 10) //Moves the character to a new room depending on the current room and aggression
                 {
                     currentRoom = 9;
                 }
@@ -149,14 +154,104 @@ namespace ics3uFinalProject
                 {
                     currentRoom = 100;
                 }
+                else if (actionSelected > 1  && currentRoom == 11)
+                {
+                    currentRoom = 300;
+                }
+                else if (actionSelected > 1 && currentRoom == 12)
+                {
+                    currentRoom = 300;
+                }
+                else if (actionSelected > 1 && currentRoom == 13)
+                {
+                    currentRoom = 100;
+                }
+                else if (actionSelected > 1 && currentRoom == 14)
+                {
+                    //Add lose here
+                }
+                else if (actionSelected > 1 && currentRoom == 15)
+                {
+                    //add lose here
+                }
+                else if (actionSelected > 2 && currentRoom == 100)
+                {
+                    currentRoom = 200;
+                }
+                else if (actionSelected > 2 && currentRoom == 200)
+                {
+                    currentRoom = 300;
+                }
+                else if (actionSelected == 2 && currentRoom == 300)
+                {
+                    currentRoom = 1;
+                }
+                else if (actionSelected > 2 && currentRoom == 300)
+                {
+                    currentRoom = 400;
+                }
+                else if (actionSelected == 2 && currentRoom == 1)
+                {
+                    currentRoom = 300;
+                }
+                else if (actionSelected > 2 && currentRoom ==1)
+                {
+                    currentRoom = 400;
+                }
+                else if (actionSelected > 1 && currentRoom == 400)
+                {
+                    //add lose here
+                }
 
-                testOutputLabel.Text += "\nMoved to room " + currentRoom.ToString() + "\n";
+
+
+                testOutputLabel.Text += "\nMoved to room " + currentRoom.ToString() + " - Vent Health : " + ventHealth.ToString() + "\n" ;
 
 
                 moveCounter = 0;
             }
             testOutputLabel.Text += moveCounter;
             moveCounter++; //Adds one second to the move counter if he doesn't move
+        }
+
+        private void ventTimer_Tick(object sender, EventArgs e)
+        {
+            //removes ventHealth every 9 seconds
+            ventHealth--;
+
+            if (ventHealth < 0 || ventHealth == 0) //Makes the monster aggressive if there's a ventilation error
+            {
+                testOutputLabel.Text += "\nVentilation Error\n";
+                aggressive = 1;
+            }
+        }
+
+        private void disableItems() //Disables everything when resetting items
+        {
+            ventButton.Enabled = false;
+        }
+
+        private void enableItems() //Enables everything after items are done being reset
+        {
+            ventButton.Enabled = true;
+        }
+
+        private async void ventButton_Click(object sender, EventArgs e)
+        {
+            ventButton.Text = "RESETTING";
+            disableItems();
+
+            Random rand = new Random();
+            int resetTime = rand.Next(5000, 8000); //Random number for reset time
+
+            // Disable other stuff here
+
+            await Task.Delay(resetTime); //Takes between 5 and 8 seconds to reset
+
+            ventButton.Text = "RESET VENTILATION";
+            enableItems();
+
+            ventHealth = 10; //Resets the vent health to full
         }
     }
 }
